e35ca21e300456f8229f9597b202e68b608a3166
Ericlefyson <ericlefysonsantana@gmail.com>
2024-03-24 16:30:54 -0300

    Fix input and add variants

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,65 +1,82 @@
-'use client'
-import React from 'react'
+"use client";
+import React from "react";
 
 import { Button } from "../ui/button";
-import { NavigationMenu,
-    NavigationMenuContent,
-    NavigationMenuIndicator,
-    NavigationMenuItem,
-    NavigationMenuLink,
-    NavigationMenuList,
-    NavigationMenuTrigger,
-    NavigationMenuViewport, } from "../ui/navigation-menu";
-import { Label } from '../ui/label';
-import { Input } from '../ui/input';
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "../ui/navigation-menu";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
 
-import { CiSearch } from "react-icons/ci";
+import { PiMagnifyingGlassThin } from "react-icons/pi";
 import Image from "next/image";
 
-export function Header(){
-
-    return(
-        <div className="flex flex-row justify-center gap-10 w-full h-36 text-black bg-white">
-            <Image alt="Logo" src="/images/logos/daddus.svg"  width={150} height={50} className="p-5"/>
-            <NavigationMenu>
-                <NavigationMenuList>
-                    <NavigationMenuItem>
-                        <NavigationMenuTrigger className="font-bold">SERVIÇOS</NavigationMenuTrigger>
-                        <NavigationMenuContent>
-                            <NavigationMenuLink>Link</NavigationMenuLink>
-                        </NavigationMenuContent>
-                    </NavigationMenuItem>
-                </NavigationMenuList>
-            </NavigationMenu>
-            <NavigationMenu>
-                <NavigationMenuList>
-                    <NavigationMenuItem>
-                        <NavigationMenuTrigger className="font-bold">SETORES</NavigationMenuTrigger>
-                        <NavigationMenuContent>
-                            <NavigationMenuLink>Link</NavigationMenuLink>
-                        </NavigationMenuContent>
-                    </NavigationMenuItem>
-                </NavigationMenuList>
-            </NavigationMenu>
-            <NavigationMenu>
-                <NavigationMenuList>
-                    <NavigationMenuItem>
-                        <NavigationMenuTrigger className="font-bold">CONTEUDOS</NavigationMenuTrigger>
-                        <NavigationMenuContent>
-                            <NavigationMenuLink>Link</NavigationMenuLink>
-                        </NavigationMenuContent>
-                    </NavigationMenuItem>
-                </NavigationMenuList>
-            </NavigationMenu>
-            <div className='flex justify-center items-center'>
-                <Label className='font-bold'>SOBRE NÓS</Label>
-            </div>
-            <div className='flex flex-row justify-center items-center p-2'>
-                <div className='flex flex-row justify-center items-center rounded-xl border border-input p-1'>
-                    <Input type="text" placeholder={"Pesquisar"}/>
-                    <CiSearch size={20} color='#A90920'/>
-                </div>
-            </div>
+export function Header() {
+  return (
+    <div className="flex flex-row justify-center gap-10 w-full h-36 text-black bg-white">
+      <Image
+        alt="Logo"
+        src="/images/logos/daddus.svg"
+        width={150}
+        height={50}
+        className="p-5"
+      />
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger className="font-bold">
+              SERVIÇOS
+            </NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <NavigationMenuLink>Link</NavigationMenuLink>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger className="font-bold">
+              SETORES
+            </NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <NavigationMenuLink>Link</NavigationMenuLink>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger className="font-bold">
+              CONTEUDOS
+            </NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <NavigationMenuLink>Link</NavigationMenuLink>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+      <div className="flex justify-center items-center">
+        <Label className="font-bold">SOBRE NÓS</Label>
+      </div>
+      <div className="flex flex-row justify-center items-center p-2">
+        <div className="flex flex-row justify-center items-center rounded-xl border border-input p-1">
+          <Input
+            iconVariant={"trailingIcon"}
+            type="text"
+            placeholder={"Pesquisar"}
+            trailingIcon={
+              <PiMagnifyingGlassThin size={30} className="fill-primary" />
+            }
+          />
         </div>
-    )
+      </div>
+    </div>
+  );
 }
--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,25 +1,54 @@
-import * as React from "react"
+import * as React from "react";
+import { cva, type VariantProps } from "class-variance-authority";
+import { cn } from "@/lib/utils";
 
-import { cn } from "@/lib/utils"
+const inputVariants = cva(
+  "flex h-10 w-full px-3 py-2 text-sm ring-offset-background focus:outline-none",
+  {
+    variants: {
+      variant: {
+        default:
+          "bg-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 !outline-none",
+        underline: "border-b border-input",
+      },
+      iconVariant: {
+        default: "",
+        trailingIcon: "pr-10",
+      },
+    },
+
+    defaultVariants: {
+      variant: "default",
+      iconVariant: "default",
+    },
+  }
+);
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+  extends React.InputHTMLAttributes<HTMLInputElement>,
+    VariantProps<typeof inputVariants> {
+  trailingIcon?: React.ReactNode;
+}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
+  ({ className, type, variant, trailingIcon, iconVariant, ...props }, ref) => {
     return (
-      <input
-        type={type}
-        className={cn(
-          "flex h-10 w-full bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
-          className
+      <div className="w-full relative">
+        <input
+          type={type}
+          className={cn(inputVariants({ variant, className, iconVariant }))}
+          ref={ref}
+          {...props}
+        />
+        {trailingIcon && (
+          <div className="absolute right-2 top-1/2 transform -translate-y-1/2">
+            {trailingIcon}
+          </div>
         )}
-        ref={ref}
-        {...props}
-      />
-    )
+      </div>
+    );
   }
-)
-Input.displayName = "Input"
+);
+Input.displayName = "Input";
 
-export { Input }
+export { Input };
