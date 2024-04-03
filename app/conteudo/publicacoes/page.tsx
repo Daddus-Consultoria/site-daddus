1f9a44a0cea723bd5b8b06a9f789e938d6229be0
Marcos Paulo <marcos.silva4@arapiraca.ufal.br>
2024-04-03 14:42:12 -0300

    feat: responsiveness adjustments

--- app/conteudo/publicacoes/page.tsx
+++ app/conteudo/publicacoes/page.tsx
@@ -10,7 +10,7 @@ const PublishPage = () => {
   return (
     <div className="flex flex-1 flex-col justify-start items-center">
         <Label className="font-bold text-[26px] lg:text-[32px] text-[#A90920] mt-11 mb-2 lg:mb-16">PUBLICAÇÕES</Label>
-        <div className="flex flex-col lg:flex-row gap-10 md:max-w-[80%] lg:max-w-[90%]">
+        <div className="flex flex-col lg:flex-row gap-10 md:max-w-[80%] lg:max-w-[90%] mb-4 lg:mb-14">
           {constantsPublications.map((item, index) => {
             return (
               <Cards
