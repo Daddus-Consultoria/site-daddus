8c1b04202018cb1335c43719efccbd2b794f02ab
Ericlefyson <ericlefysonsantana@gmail.com>
2024-04-09 22:13:04 -0300

    Merge

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -31,6 +31,7 @@ const config = {
     },
     extend: {
       colors: {
+        lightGray: "hsl(var(--light-gray))",
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
