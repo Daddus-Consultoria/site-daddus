b86fcbe325ed9011524329de30056549963b05cb
Ericlefyson <ericlefysonsantana@gmail.com>
2024-05-12 18:16:49 -0300

    Fix lint again

--- lib/utils/transformDataToInterface.ts
+++ lib/utils/transformDataToInterface.ts
@@ -1,12 +1,10 @@
 import { AuthorModel } from "@/lib/interfaces/author";
+import { mapperAuthor } from "@/lib/services/author/index";
 
 export const getAuthorsList = (data: any[]): AuthorModel[] => {
   try {
     return data.map((author) => {
-      return {
-        id: author.id,
-        name: author.attributes.name,
-      };
+      return mapperAuthor(author);
     });
   } catch (error) {
     console.error(`Error getting authors list: ${error}`);
