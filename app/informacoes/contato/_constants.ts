3929e3e9fadbabb43dba3bb4238a032e777ae143
Marcos Paulo <marcos.silva4@arapiraca.ufal.br>
2024-05-21 10:34:12 -0300

    feat: start of contact screen

---
+++ app/informacoes/contato/_constants.ts
@@ -0,0 +1,9 @@
+
+export const constantsContact = {
+    image: '/images/logos/daddusSquare.svg',
+    title: 'Entre em contato',
+    text: 'Para contratar nossos serviços, entre em contato com a nossa equipe de consultores por e-mail. O atendimento é realizado de segunda a sexta-feira, das 9h às 17h:',
+    email: 'suporte@daddusconsultoria.com',
+    text2: 'Opiniões, dúvidas, sugestões e perguntas sobre os serviços Daddus podem ser enviadas por meio do formulário abaixo.',
+}
+
