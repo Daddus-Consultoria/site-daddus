1aa452426526b70448677065a198ddf59cf7b1c8
Marcos Paulo <marcos.silva4@arapiraca.ufal.br>
2024-05-22 10:48:51 -0300

    fix: adjustments var

--- components/avatarNetwork/index.tsx
+++ components/avatarNetwork/index.tsx
@@ -14,12 +14,12 @@ interface AvatarNetworkProps {
 const AvatarNetwork: React.FC<AvatarNetworkProps> = ({ className }) => {
   const urlPath = usePathname();
 
-  console.log(urlPath)
+  const url = encodeURIComponent(Links.SITE_DOMAIN + urlPath);
 
   return (
     <div className={cn("flex flex-row gap-[10px]", className)}>
       {avatarNetworks.map((item, index) => (
-        <Link key={`avatarNetwork-${index}`} href={`${item.path+encodeURIComponent(Links.SITE_DOMAIN+urlPath)}`} target="_blank">
+        <Link key={`avatarNetwork-${index}`} href={`${item.path+url}`} target="_blank">
           <Avatar className="flex  hover:shadow-xl transition duration-300 ">
             <AvatarImage src={item.src} />
             <AvatarFallback>{item.fallback}</AvatarFallback>
