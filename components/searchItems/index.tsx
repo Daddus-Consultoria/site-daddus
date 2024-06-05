59d9e302d6100db3f55f82ea54941b3fea3a7d35
Marcos Paulo <marcos.silva4@arapiraca.ufal.br>
2024-06-05 18:55:10 -0300

    fix: adjustment of the search component - now the search is being done, however, in a mock

--- components/searchItems/index.tsx
+++ components/searchItems/index.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useEffect, useState } from 'react';
+/* import { ChangeEvent, useEffect, useRef, useState } from 'react';
 
 import {Button, Command,
     CommandDialog,
@@ -33,6 +33,7 @@ const SearchItems = () => {
 
     const [open, setOpen] = useState(false)
     const [value, setValue] = useState("")
+    const inputRef = useRef<HTMLInputElement>(null)
 
     const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
       const value = event.target.value
@@ -46,6 +47,15 @@ const SearchItems = () => {
       }
     }
 
+    const handleInputClick = () => {
+      if(!open){
+        setOpen(true)
+      }
+      if(inputRef.current){
+        inputRef.current.focus() 
+      }
+    }
+
     return (
         <Popover open={open}>
           <PopoverTrigger asChild>
@@ -55,11 +65,8 @@ const SearchItems = () => {
                   type="text"
                   placeholder={value ? value : "Pesquisar"}
                   className="bg-primary text-white placeholder:text-white"
-                  onClick={()=>{
-                    if(!open){
-                      setOpen(true)
-                    }
-                  }}
+                  onClick={handleInputClick}
+                  ref={inputRef}
                   onChange={handleInputChange}
                   trailingIcon={
                     <PiMagnifyingGlassThin
@@ -97,4 +104,103 @@ const SearchItems = () => {
     );
 } 
 
-export {SearchItems}
+export {SearchItems} */
+
+import { Input } from "@/components/index";
+
+import { PiMagnifyingGlassThin } from "react-icons/pi";
+
+import React, { useState, useRef, useEffect } from 'react';
+
+const SearchItems: React.FC = () => {
+    
+  const frameworks = [
+    "Next.js", "Nuxt.js", "SvelteKit", "Remix", "Astro"
+  ]
+
+  const [isPopoverOpen, setIsPopoverOpen] = useState(false);
+  const [query, setQuery] = useState('');
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [filteredItems, setFilteredItems] = useState<string[]>(frameworks)
+
+  const handleInputClick = () => {
+    setIsPopoverOpen(true);
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setQuery(value);
+
+    const filtered = frameworks.filter((framework)=>framework.toLowerCase().includes(value.toLowerCase()));
+      setFilteredItems(filtered)
+  };
+
+  const closePopover = () => {
+    setIsPopoverOpen(false);
+  };
+
+
+
+  const handleInputBlur = () => {
+    setTimeout(()=>{
+      if(!inputRef.current?.contains(document.activeElement)){
+        setIsPopoverOpen(false);
+      }
+    })
+  }
+
+  return (
+    <div className="relative w-64">
+      <div className="flex flex-row justify-center items-center rounded-xl border border-input p-1">
+        <Input
+            type="text"
+            /* className="w-full border bg-primary border-gray-300 text-white rounded-md p-2" */
+            className="bg-primary text-white placeholder:text-white"
+            value={query}
+            onClick={handleInputClick}
+            onChange={handleInputChange}
+            ref={inputRef}
+            onBlur={handleInputBlur}
+            placeholder="Search..."
+            trailingIcon={
+              <PiMagnifyingGlassThin
+                size={30}
+                className="fill-secondary"
+                color="#2B2B2B"
+              />
+            }
+          />
+        </div>
+      {/* <input
+        type="text"
+        className="w-full border border-gray-300 rounded-md p-2"
+        value={query}
+        onClick={handleInputClick}
+        onChange={handleInputChange}
+        ref={inputRef}
+        onBlur={handleInputBlur}
+        placeholder="Search..."
+      /> */}
+      {isPopoverOpen && (
+        <div className="absolute z-10 mt-1 w-full bg-white border border-gray-300 rounded-md py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+          <ul>
+            {filteredItems.map((framework) => (
+              <li 
+                className="cursor-pointer select-none py-2 pl-10 pr-4 hover:bg-blue-600 hover:text-white"
+                onMouseDown={()=>setQuery(framework)}
+              >
+                {framework}
+              </li>
+            ))}
+
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export {SearchItems};
