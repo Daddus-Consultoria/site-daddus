205354864f60204f12d592ef19908fd270ad7eea
adautobms <abms1@aluno.ifal.edu.br>
2024-06-20 19:15:59 -0300

    adjusts the layout of the image and ad on small screens on the consultancy pages

--- app/servicos/consultoria/modelagem-projetos/page.tsx
+++ app/servicos/consultoria/modelagem-projetos/page.tsx
@@ -4,99 +4,122 @@ import Image from "next/image";
 import { Contact, RelatedPublications, CardInfo } from "@/components/index";
 
 const PublicPoliciesPage = () => {
-  const relatedPublications = [
-    {
-      title: "Citolologia: um estudo demográfico de  duas ou três linhas ",
-      link: "#",
-    },
-    {
-      title:
-        "Perspectivas de Mercado: Bitcoin e o mercado bovino. Onde vamos parar?",
-      link: "#",
-    },
-  ];
+    const relatedPublications = [
+        {
+            title: "Citolologia: um estudo demográfico de  duas ou três linhas ",
+            link: "#",
+        },
+        {
+            title:
+                "Perspectivas de Mercado: Bitcoin e o mercado bovino. Onde vamos parar?",
+            link: "#",
+        },
+    ];
 
-  return (
-    <div className="flex flex-1 flex-col">
-        <div
-            id="top-transport-page"
-            className="flex flex-col lg:flex-row w-full h-full py-[4%] px-[9%]  gap-[2%] lg:gap-[5%] "
-        >
-            <div id="left" className="flex lg:w-1/2 flex-col">
-                {constantsProjectModeling.content.map((item, index) => (
-                    <div
-                    key={`transport-page-${index}`}
-                    className="flex flex-col mb-[8%]"
-                    >
-                        <h2 className="font-bold text-[26px] lg:text-[32px] text-primary mb-[2%] ">
-                            {item.title}
-                        </h2>
-                        <p className="flex text-justify text-[17px] text-[#696984] leading-loose whitespace-pre-line ">
-                            {item.text}
-                        </p>
-                        {item.listConsultancy && (
-                            <div>
-                                <ol className={`list-${item.type}`}>
-                                    {item.listConsultancy.map((item, index) => (
-                                        <li key={`list-consultancy-elaboration-policy-page`} className="text-[17px] text-[#696984] ml-[5%]">  
-                                            <p className="flex text-justify text-[17px] text-[#696984] leading-loose whitespace-pre-line ">
-                                                {item}
-                                            </p>
-                                        </li>
-                                    ))}
-                                </ol>
-                            </div>
-                            
-                        )}
-                    </div>
-                ))}
-            </div>
-            <div className="lg:hidden w-full px-[10px] bg-[#D6D6D6] py-[0.5px]"></div>
+    return (
+        <div className="flex flex-1 flex-col">
+            {/* Top Section */}
             <div
-                id="right"
-                className="flex lg:w-1/2 flex-col justify-start items-end gap-[7%] "
+                id="top-transport-page"
+                className="flex flex-col lg:flex-row w-full h-full py-[4%] px-[9%] gap-[2%] lg:gap-[5%]"
             >
-                <Image
-                    src={constantsProjectModeling.image.src}
-                    width={450}
-                    height={300}
-                    alt={constantsProjectModeling.image.alt}
-                />
-                <div className="flex justify-center items-center bg-[#D9D9D9] h-[400px] w-[300px]">
-                    <p className="font-bold text-[26px] lg:text-[32px] text-primary mb-[2%]">
-                    GOOGLE
-                    </p>
+                {/* Image Above for Small Screens */}
+                <div className="lg:hidden mb-[4%]">
+                    <Image
+                        src={constantsProjectModeling.image.src}
+                        width={450}
+                        height={300}
+                        alt={constantsProjectModeling.image.alt}
+                        className="mx-auto"
+                    />
                 </div>
-                <div className="flex flex-row w-full justify-end">
-                    <div className="flex lg:w-[72%] flex-col gap-10 mb-[35%] md:mb-[15%]">
-                    <Contact />
-                    <RelatedPublications publicationsRelated={relatedPublications} />
-                    </div>
+
+                {/* Left Column */}
+                <div id="left" className="flex lg:w-1/2 flex-col">
+                    {constantsProjectModeling.content.map((item, index) => (
+                        <div key={`transport-page-${index}`} className="flex flex-col mb-[8%]">
+                            <h2 className="font-bold text-[26px] lg:text-[32px] text-primary mb-[2%]">
+                                {item.title}
+                            </h2>
+                            <p className="flex text-justify text-[17px] text-[#696984] leading-loose whitespace-pre-line">
+                                {item.text}
+                            </p>
+                            {/* Insert Ad after the first paragraph */}
+                            {index === 0 && (
+                                <div className="lg:hidden flex justify-center items-center bg-[#D9D9D9] my-[4%] h-[200px] w-full">
+                                    <p className="font-bold text-[26px] lg:text-[32px] text-primary">
+                                        GOOGLE
+                                    </p>
+                                </div>
+                            )}
+                            {item.listConsultancy && (
+                                <div>
+                                    <ol className={`list-${item.type}`}>
+                                        {item.listConsultancy.map((listItem, listIndex) => (
+                                            <li key={`list-consultancy-elaboration-policy-page-${listIndex}`} className="text-[17px] text-[#696984] ml-[5%]">
+                                                <p className="flex text-justify text-[17px] text-[#696984] leading-loose whitespace-pre-line">
+                                                    {listItem}
+                                                </p>
+                                            </li>
+                                        ))}
+                                    </ol>
+                                </div>
+                            )}
+                        </div>
+                    ))}
                 </div>
-            </div>
-        </div>
-        <div id="bottom-transport-page" className="px-[7%]">
-            <h2 className=" px-[2%] font-bold text-[26px] lg:text-[32px] text-primary mb-[2%] ">
-            {constantsProjectModeling.footer.title}
-            </h2>
-            <div className="w-full bg-[#999999] h-[1.5px]"></div>
-            <div className="flex flex-col lg:flex-row gap-10 lg:h-[500px] 2xl:h-[500px] pb-20 md:justify-center md:items-center lg:justify-start lg:items-start">
-                {constantsProjectModeling.footer.cards.map((item, index) => (
-                    <div key={`card-transport-page-${index}`} className="flex flex-col lg:flex-row gap-10 h-full md:max-w-[70%] lg:max-w-[33%] mb-4 lg:mb-14 mt-[3%] rounded-3xl shadow-xl">
-                        <CardInfo
-                            title={item.title}
-                            description={item.description}
-                            image={item.image}
-                            path={item.path}
-                            copyLink={item.copyLink}
-                            titleAlign={item.titleAlign}
+
+                {/* Divider for small screens */}
+                <div className="lg:hidden w-full px-[10px] bg-[#D6D6D6] py-[0.5px]"></div>
+
+                {/* Right Column */}
+                <div id="right" className="flex lg:w-1/2 flex-col justify-start items-end gap-[7%]">
+                    <div className="hidden lg:block">
+                        <Image
+                            src={constantsProjectModeling.image.src}
+                            width={450}
+                            height={300}
+                            alt={constantsProjectModeling.image.alt}
                         />
                     </div>
-                ))}
+                    <div className="hidden lg:flex justify-center items-center bg-[#D9D9D9] h-[400px] w-[300px]">
+                        <p className="font-bold text-[26px] lg:text-[32px] text-primary mb-[2%]">
+                            GOOGLE
+                        </p>
+                    </div>
+                    <div className="flex flex-row w-full justify-end">
+                        <div className="flex lg:w-[72%] flex-col gap-10 mb-[35%] md:mb-[15%]">
+                            <Contact />
+                            <RelatedPublications publicationsRelated={relatedPublications} />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* Bottom Section */}
+            <div id="bottom-transport-page" className="px-[7%]">
+                <h2 className="px-[2%] font-bold text-[26px] lg:text-[32px] text-primary mb-[2%]">
+                    {constantsProjectModeling.footer.title}
+                </h2>
+                <div className="w-full bg-[#999999] h-[1.5px]"></div>
+                <div className="flex flex-col lg:flex-row gap-10 lg:h-[500px] 2xl:h-[500px] pb-20 md:justify-center md:items-center lg:justify-start lg:items-start">
+                    {constantsProjectModeling.footer.cards.map((item, index) => (
+                        <div key={`card-transport-page-${index}`} className="flex flex-col lg:flex-row gap-10 h-full md:max-w-[70%] lg:max-w-[33%] mb-4 lg:mb-14 mt-[3%] rounded-3xl shadow-xl">
+                            <CardInfo
+                                title={item.title}
+                                description={item.description}
+                                image={item.image}
+                                path={item.path}
+                                copyLink={item.copyLink}
+                                titleAlign={item.titleAlign}
+                            />
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
-    </div>
-  );
+
+    );
 };
 
 export default PublicPoliciesPage;
