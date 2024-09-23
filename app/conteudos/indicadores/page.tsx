720f9a04d20fefce728c6f191887c3c17ecaa621
Ericlefyson <ericlefysonsantana@gmail.com>
2024-09-23 07:46:19 -0300

    Temporaly hide the requests

--- app/conteudos/indicadores/page.tsx
+++ app/conteudos/indicadores/page.tsx
@@ -4,10 +4,12 @@ import { ChartUseCases } from "@/lib/useCases/chartUseCases";
 
 export default async function IndicatorsPage() {
   const useChartCase = new ChartUseCases();
-  const [graphicData, mapData] = await Promise.all([
-    useChartCase.gettAllIndicatorsDaddusGraphData(),
-    useChartCase.getAllIndicatorsStateChartData(),
-  ]);
+  // const [graphicData, mapData] = await Promise.all([
+  //   useChartCase.gettAllIndicatorsDaddusGraphData(),
+  //   useChartCase.getAllIndicatorsStateChartData(),
+  // ]);
+  const graphicData:any[] = [];
+  const mapData:any[] = [];
 
   return (
     <div>
