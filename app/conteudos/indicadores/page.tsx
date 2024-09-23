c92e2ec999ad53d159071c829afea0ee241c9361
Ericlefyson <ericlefysonsantana@gmail.com>
2024-09-23 07:36:17 -0300

    Format code

--- app/conteudos/indicadores/page.tsx
+++ app/conteudos/indicadores/page.tsx
@@ -2,18 +2,16 @@ import { IndicatorsMenuPage } from "@/components/index";
 
 import { ChartUseCases } from "@/lib/useCases/chartUseCases";
 
-export default async function IndicatorsPage () {
+export default async function IndicatorsPage() {
   const useChartCase = new ChartUseCases();
   const [graphicData, mapData] = await Promise.all([
     useChartCase.gettAllIndicatorsDaddusGraphData(),
     useChartCase.getAllIndicatorsStateChartData(),
   ]);
 
-
   return (
     <div>
-      <IndicatorsMenuPage graphicData={graphicData} mapData={mapData}/>
+      <IndicatorsMenuPage graphicData={graphicData} mapData={mapData} />
     </div>
-  )
-};
-
+  );
+}
