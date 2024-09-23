6034198b207b462b4864c5f431ddabf2954d682e
Ericlefyson <ericlefysonsantana@gmail.com>
2024-09-23 08:01:59 -0300

    Try new request form

--- lib/services/chart/chartAPIService.ts
+++ lib/services/chart/chartAPIService.ts
@@ -23,10 +23,11 @@ export class ChartAPIService implements ChartUseCases, ChartRepository {
   }
 
   async getAllIndicatorsStateChartData() {
-    const res = await fetch(`${this.apiURL}/state-map-charts`);
-    const data = await res.json();
+    // const res = await fetch(`${this.apiURL}/state-map-charts`);
+    // const data = await res.json();
+    // return this.formatMapData(data);
 
-    /* const auth = await google.auth.getClient({
+    const auth = await google.auth.getClient({
       projectId: process.env.GOOGLE_SHEETS_PROJECT_ID,
       credentials: {
         type: "service_account",
@@ -46,36 +47,48 @@ export class ChartAPIService implements ChartUseCases, ChartRepository {
       spreadsheetId:process.env.GOOGLE_SHEETS_MAP_CHART_SPREADSHEET_ID,
       range: range,
     });
-  
-    res.status(200).json(data.data.values); */
 
-    return this.formatMapData(data);
+    return this.formatMapData(data?.data?.values || []);
   }
 
   async gettAllIndicatorsDaddusGraphData(): Promise<DataSpreadSheetsGraphic[]> {
-    const res = await fetch(`${this.apiURL}/api/state-graphic-charts`); // colocar a rota https://daddus.....
+    // const res = await fetch(`${this.apiURL}/api/state-graphic-charts`); // colocar a rota https://daddus.....
 
-    const data = await res.json();
+    // const data = await res.json();
 
-    const resultAll = data.slice(1).map((item: string[]) => {
+    const auth = await google.auth.getClient({
+      projectId: process.env.GOOGLE_SHEETS_PROJECT_ID,
+      credentials: {
+        type: "service_account",
+        private_key: process.env.GOOGLE_SHEETS_PRIVATE_KEY,
+        client_email: process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
+        client_id: process.env.GOOGLE_SHEETS_CLIENT_ID,
+        token_url: "https://oauth2.googleapis.com/token",
+        universe_domain: "googleapis.com",
+      },
+      scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
+    });
+  
+    const sheets = google.sheets({ version: "v4", auth });
+    const range = "IPCA!A1:Z1000";
+  
+    const data = await sheets.spreadsheets.values.get({
+      spreadsheetId:process.env.GOOGLE_SHEETS_GRAPHIC_CHART_SPREADSHEET_ID,
+      range: range,
+    });
+
+    const formatedData = data?.data?.values || [];
+    
+
+    const resultAll = formatedData.slice(1).map((item: string[]) => {
        return {
         dataGraphic: [
-          //['Ano', 'Porcentagem'],
           ...item,
         ],
       } 
-      //return transformData(item);
     })
 
-    /* {
-      dataGraph: [
-        ['Ano', 'Porcentagem'],
-        ...resultAll,
-      ],
-      dataSelectors:{
-        uf: data[5]
-      }
-    } */
+
 
     return resultAll;
   }
