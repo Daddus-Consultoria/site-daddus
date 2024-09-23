5db0b22483bedbea6107836e24a2e5f891db5d9e
Ericlefyson <ericlefysonsantana@gmail.com>
2024-09-23 08:21:20 -0300

    use server action

--- app/conteudos/indicadores/page.tsx
+++ app/conteudos/indicadores/page.tsx
@@ -4,10 +4,16 @@ import { ChartUseCases } from "@/lib/useCases/chartUseCases";
 
 export default async function IndicatorsPage() {
   const useChartCase = new ChartUseCases();
+  const getData = async () => {
+  "use server";
   const [graphicData, mapData] = await Promise.all([
     useChartCase.gettAllIndicatorsDaddusGraphData(),
     useChartCase.getAllIndicatorsStateChartData(),
   ]);
+  return { graphicData, mapData };
+  }
+
+  const { graphicData, mapData } = await getData();
 
   return (
     <div>
